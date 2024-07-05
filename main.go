aa92a147f38b2e8e414e89baa9c7b02fbf0c9db8
Abi Liu <abiliu018@gmail.com>
2024-07-05 01:01:40 -0700

    got repl working

--- main.go
+++ main.go
@@ -1,32 +1,57 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
+
+type cliCommand struct {
+	name        string
+	description string
+	callback    func() error
+}
 
 func main() {
+	s := bufio.NewScanner(os.Stdin)
+	for {
+		s.Scan()
+		text := s.Text()
+		command := cleanInput(text)
+		fmt.Print(command)
+	}
+}
 
-	a := make([]int, 3)
-	fmt.Println("len of a:", len(a))
-	// len of a: 3
-	fmt.Println("cap of a:", cap(a))
-	// cap of a: 3
-	fmt.Println("appending 4 to b from a")
-	// appending 4 to b from a
-	b := append(a, 4)
-	fmt.Println("b:", b)
-	// b: [0 0 0 4]
-	fmt.Println(cap(b))
-	fmt.Println("addr of b:", &b[0])
-	// addr of b: 0x44a0c0
-	fmt.Println("appending 5 to c from a")
-	// appending 5 to c from a
-	c := append(a, 5)
-	fmt.Println("addr of c:", &c[0])
-	// addr of c: 0x44a180
-	fmt.Println(cap(c))
-	fmt.Println("a:", a)
-	// a: [0 0 0]
-	fmt.Println("b:", b)
-	// b: [0 0 0 4]
-	fmt.Println("c:", c)
-	// c: [0 0 0 5]
+func cleanInput(command string) string {
+	lowercaseText := strings.ToLower(command)
+	slice := strings.Fields(lowercaseText)
+	return strings.Join(slice, " ")
+}
+
+func getCliCommands() map[string]cliCommand {
+
+	return map[string]cliCommand{
+		"help": {
+			name:        "help",
+			description: "Display a help message",
+			callback:    commandHelp,
+		},
+		"exit": {
+			name:        "exit",
+			description: "Exit the Pokedex",
+			callback:    commandExit,
+		},
+	}
+}
+
+func commandExit() error {
+	return nil
+}
+
+func commandHelp() error {
+	for k, v := range getCliCommands() {
+		fmt.Printf("%s - %s\n", k, v.description)
+	}
+	return nil
 }
